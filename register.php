c3d27ec663b160a4ce93a4b2cd89277ba55817c9
Uni <a5304186@gmail.com>
2019-06-19 23:03:11 +0800

    sign up

--- register.php
+++ register.php
@@ -54,15 +54,54 @@
     </nav>
     <div class="container container--col" style="justify-content: center;align-items: center">
         <h2>註冊</h2>
-        <form action="#" method="POST" style="width: 30%;">
+        <form action="register.php" method="POST" style="width: 30%;">
             <label for="username">名稱</label>
             <input type="text" name="username" id="username" /><br>
             <label for="useremail">信箱</label>
             <input type="email" name="useremail" id="useremail" /><br>
             <label for="userpassword">密碼</label>
             <input type="password" name="userpassword" id="userpassword" /><br>
+            <?php
+            // 檢查欄位是否空白
+            if(isset($_POST['username']) && isset($_POST['useremail']) && isset($_POST['userpassword'])){
+                $name = $_POST['username'];
+                $email = $_POST['useremail'];
+                $password = $_POST['userpassword'];
+
+                // 連接資料庫
+                $conn = mysqli_connect('localhost','uni','uni0110','foodmap');
+                if(!$conn){
+                    die('Could not connect: '.mysqli_connect_error());
+                }
+
+                // 尋找是否有相同名字的用戶和信箱
+                $sql = 'select * from Users where Username="'.$name.'" or Email="'.$email.'";';
+                $result = mysqli_query($conn, $sql);
+                if(!$result){
+                    die('Error: '.mysqli_error($conn));
+                }
+
+                $rows = mysqli_fetch_assoc($result);
+                if(!$rows){
+                    // 新增使用者
+                    $sql = 'insert into Users values("'.$name.'","'.$email.'","'.$password.'","'.date('Y-m-d').'");';
+                    $result = mysqli_query($conn, $sql);
+                    if(!$result){
+                        die('Error: '.mysqli_error($conn));
+                    }
+
+                    header('location:login.php');
+                    exit();
+                }
+                else{
+                    echo '<p style="color: red">註冊失敗，請再試一次</p><br>';
+                }
+            }
+
+            mysqli_close();
+            ?>
             <p>已經有帳戶？ <a href="./login.php" style="display: inline">登入</a></p>
-            <input type="submit" id="register" value="註冊" />
+            <input type="submit" id="register" name="register" value="註冊" />
         </form>
     </div>
 </body>
